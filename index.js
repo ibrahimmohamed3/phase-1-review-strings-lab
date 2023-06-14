5cb016b2659b4f76aca780d4c81fa0d2a3b3c204
ibrahimmohamed3 <ibra39167271@gmail.com>
2023-06-14 15:12:19 +0300

    completed assignmen~t

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+
+const currentUser = 'Ibrahim Mohamed';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
+
